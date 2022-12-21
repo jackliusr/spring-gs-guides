165a31f2134e6d790e685c3b4521e2b86d03ed72
Jack Liu <jackliusr@gmail.com>
2022-12-21 12:30:26 +0800

    init of Accessing Data with MongoDB

---
+++ accessingdatamongodb/src/main/java/com/example/accessingdatamongodb/AccessingdatamongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingdatamongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingdatamongodbApplication.class, args);
+	}
+
+}
---
+++ accessingdatamongodb/src/test/java/com/example/accessingdatamongodb/AccessingdatamongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingdatamongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
