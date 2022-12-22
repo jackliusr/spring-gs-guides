ada463558885a782f3872c65e35a1d51c9b6bee8
Jack Liu <jackliusr@gmail.com>
2022-12-22 22:10:56 +0800

    init of Enabling Cross Origin Requests for a RESTful Web Service

---
+++ restservicecors/src/main/java/com/example/restservicecors/RestservicecorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.restservicecors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestservicecorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestservicecorsApplication.class, args);
+	}
+
+}
