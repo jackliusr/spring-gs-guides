561df63043d4270dd8c3d1799b07c53267165ed6
Jack Liu <jackliusr@gmail.com>
2022-12-23 22:04:48 +0800

    Accessing Neo4j Data with REST

---
+++ accessingneo4jdatarest/src/main/java/com/example/accessingneo4jdatarest/Accessingneo4jdatarestApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessingneo4jdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Accessingneo4jdatarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Accessingneo4jdatarestApplication.class, args);
+	}
+
+}
---
+++ accessingneo4jdatarest/src/test/java/com/example/accessingneo4jdatarest/Accessingneo4jdatarestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingneo4jdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Accessingneo4jdatarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
