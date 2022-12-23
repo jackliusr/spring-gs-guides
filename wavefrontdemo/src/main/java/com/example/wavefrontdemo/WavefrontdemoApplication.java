17f4bee8b2df9c9ab21fdf6f5f455599b4a327f2
Jack Liu <jackliusr@gmail.com>
2022-12-23 19:59:53 +0800

    init of Observability with Spring

---
+++ wavefrontdemo/src/main/java/com/example/wavefrontdemo/WavefrontdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wavefrontdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavefrontdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavefrontdemoApplication.class, args);
+	}
+
+}
---
+++ wavefrontdemo/src/test/java/com/example/wavefrontdemo/WavefrontdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wavefrontdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavefrontdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
