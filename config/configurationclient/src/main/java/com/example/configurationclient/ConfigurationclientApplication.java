69f589ce153a86cc59f30791338133e671efc5f8
Jack Liu <jackliusr@gmail.com>
2022-12-25 23:33:43 +0800

    init of Centralized Configuration

---
+++ config/configurationclient/src/main/java/com/example/configurationclient/ConfigurationclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.configurationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationclientApplication.class, args);
+	}
+
+}
