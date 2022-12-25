928883a533aef3c41652eca55a2a86f07f74ca69
Jack Liu <jackliusr@gmail.com>
2022-12-25 20:18:24 +0800

    init of Service Registration and Discovery

---
+++ Eureka/serviceregistrationanddiscoveryclient/src/main/java/com/example/serviceregistrationanddiscoveryclient/ServiceregistrationanddiscoveryclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceregistrationanddiscoveryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceregistrationanddiscoveryclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceregistrationanddiscoveryclientApplication.class, args);
+	}
+
+}
---
+++ Eureka/serviceregistrationanddiscoveryservice/src/main/java/com/example/serviceregistrationanddiscoveryservice/ServiceregistrationanddiscoveryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceregistrationanddiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceregistrationanddiscoveryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceregistrationanddiscoveryserviceApplication.class, args);
+	}
+
+}
