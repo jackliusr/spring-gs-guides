77de8e4bb133085a55eae1556c508c273765fc0a
Jack Liu <jackliusr@gmail.com>
2022-12-26 20:11:41 +0800

    complete of Building a Reactive RESTful Web Service

--- reactiverest/src/main/java/com/example/reactiverest/ReactiverestApplication.java
+++ reactiverest/src/main/java/com/example/reactiverest/ReactiverestApplication.java
@@ -2,12 +2,16 @@ package com.example.reactiverest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ReactiverestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReactiverestApplication.class, args);
+		ConfigurableApplicationContext context= SpringApplication.run(ReactiverestApplication.class, args);
+	    GreetingClient greetingClient = context.getBean(GreetingClient.class);
+	    // We need to block for the content here or the JVM might exit before the message is logged
+	    System.out.println(">> message = " + greetingClient.getMessage().block());
 	}
 
 }
--- reactiverest/src/test/java/com/example/reactiverest/ReactiverestApplicationTests.java
+++ reactiverest/src/test/java/com/example/reactiverest/ReactiverestApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.reactiverest;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ReactiverestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
+	  // Spring Boot will create a `WebTestClient` for you,
+	  // already configure and ready to issue requests against "localhost:RANDOM_PORT"
+	  @Autowired
+	  private WebTestClient webTestClient;
+
+	  @Test
+	  public void testHello() {
+	    webTestClient
+	      // Create a GET request to test an endpoint
+	      .get().uri("/hello")
+	      .accept(MediaType.APPLICATION_JSON)
+	      .exchange()
+	      // and use the dedicated DSL to test assertions against the response
+	      .expectStatus().isOk()
+	      .expectBody(Greeting.class).value(greeting -> {
+	        assertThat(greeting.getMessage()).isEqualTo("Hello, Spring!");
+	    });
+	  }
 }
