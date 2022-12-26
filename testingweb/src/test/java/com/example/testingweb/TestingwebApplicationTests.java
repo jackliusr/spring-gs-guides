b0b9148bae2451fcd1781dbd0b87b13f47728f92
Jack Liu <jackliusr@gmail.com>
2022-12-26 15:08:40 +0800

    complete of Testing the Web Layer

--- testingweb/src/test/java/com/example/testingweb/TestingwebApplicationTests.java
+++ testingweb/src/test/java/com/example/testingweb/TestingwebApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.testingweb;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TestingwebApplicationTests {
-
+	
+	@Autowired
+	private HomeController controller;
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello, World")));
 	}
 
 }
